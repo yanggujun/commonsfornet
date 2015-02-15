2527a3879e69c70ab07dc488c74adbe4422f3b42
yanggujun <gujun.yang@gmail.com>
2015-02-15 21:45:35 +0800

    add build script to create nuget package

--- Build.bat
+++ Build.bat
@@ -1,6 +1,8 @@
 set PATH=%PATH%;.\src\.nuget
 call "%VS120COMNTOOLS%\vsvars32.bat"
+call rmdir /S /Q ".\src\bin"
 call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;TragetFrameworkVersion=4.5.1
 call sn -R .\src\bin\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Json.dll .\src\FullKey.snk
+call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"
