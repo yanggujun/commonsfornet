87defb930e4040ffd453d6bf5d81b8d237ffdecf
yanggujun <gujun.yang@gmail.com>
2015-02-25 18:36:09 +0800

    update copy right information

--- src/Commons.Collections/Map/IReadOnlyBimap.cs
+++ src/Commons.Collections/Map/IReadOnlyBimap.cs
@@ -1,4 +1,4 @@
-﻿// Copyright CommonsForNET 2014.
+﻿// Copyright CommonsForNET.
 // Licensed to the Apache Software Foundation (ASF) under one or more
 // contributor license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright ownership.
--- src/Commons.Collections/Map/MapEnumerator.cs
+++ src/Commons.Collections/Map/MapEnumerator.cs
@@ -1,4 +1,4 @@
-﻿// Copyright CommonsForNET 2014.
+﻿// Copyright CommonsForNET.
 // contributor license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright ownership.
 // The ASF licenses this file to You under the Apache License, Version 2.0
