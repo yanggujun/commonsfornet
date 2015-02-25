aaee5a2e8eabc5afae124fb71235620daa0821f7
yanggujun <gujun.yang@gmail.com>
2015-02-25 18:36:47 +0800

    add commons pool project

---
+++ src/Commons.Patterns/Pool/IObjectPool.cs
@@ -0,0 +1,41 @@
+﻿// Copyright CommonsForNET.
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Commons.Patterns.Pool
+{
+	/// <summary>
+	/// The interface defines the operations for an object pool.
+	/// </summary>
+	/// <typeparam name="T">The type of the pooled object.</typeparam>
+	public interface IObjectPool<T>
+	{
+		/// <summary>
+		/// Acquires an object from the pool. 
+		/// If there is any object available in the pool, one of the idle objects is returned
+		/// from the pool. And the idle object is marked as allocated.
+		/// If there is not any idle object in the pool, and the pool size is less than the max value,
+		/// the object pool attempts to create an object and returns the object if successful. Pool size is increased.
+		/// If there is not any idle object in the pool and the pool size has reached the max value, 
+		/// <see cref="ResourceUnavailableException"/> is thrown.
+		/// </summary>
+		/// <returns>The object</returns>
+		T Acquire();
+
+		void Return(T obj);
+
+		bool ValidateObject(T obj);
+	}
+}
