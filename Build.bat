e94f4cfdee2cd395a17070c2aef8181a085e4a31
yanggujun <gujun.yang@gmail.com>
2015-02-27 18:39:59 +0800

    The Commons.Utils, Commons.Collections and Commons.Json currently are not separated into different nuget packages. To go ahead, Commons.Patterns will be a separate nuget package.

--- Build.bat
+++ Build.bat
@@ -5,7 +5,5 @@ call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;TragetFramework
 call sn -R .\src\bin\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Json.dll .\src\FullKey.snk
-call nuget pack Commons.Utils.nuspec -OutputDirectory ".\src\bin"
-call nuget pack Commons.Collections.nuspec -OutputDirectory ".\src\bin"
-call nuget pack Commons.Json.nuspec -OutputDirectory ".\src\bin"
+call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"
 call PAUSE
