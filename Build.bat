52cb21588e971104b7a58b18f7fd83ee184a3353
yanggujun <gujun.yang@gmail.com>
2015-03-10 22:00:51 +0800

    add Commons.Pool nuspec

--- Build.bat
+++ Build.bat
@@ -5,6 +5,8 @@ call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;TragetFramework
 call sn -R .\src\bin\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\Commons.Json.dll .\src\FullKey.snk
+call sn -R .\src\bin\Commons.Pool.dll .\src\FullKey.snk
 call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\Test.Commons.dll /xml src\bin\UnitTestResult.xml
 call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"
+call nuget pack Commons.Pool.nuspec -OutputDirectory ".\src\bin"
 call PAUSE
