254e04e9026b64027948ade4e4c03c694ba997c6
Alan Yang <gujun.yang@carestream.com>
2015-06-09 13:58:12 +0800

    build for .net 4.0 as well as .net 4.5

--- Build.bat
+++ Build.bat
@@ -2,13 +2,13 @@ set PATH=%PATH%;.\src\.nuget
 call "%VS120COMNTOOLS%\vsvars32.bat"
 call rmdir /S /Q ".\src\bin"
 
-call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";TragetFrameworkVersion=4.5;Framework=NET45
+call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";Framework=NET45
 call sn -R .\src\bin\NET45\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Json.dll .\src\FullKey.snk
 call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\NET45\Test.Commons.dll /xml src\bin\NET45\UnitTestResult.xml
 
-call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";TragetFrameworkVersion=4.0;Framework=NET40
+call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";Framework=NET40
 call sn -R .\src\bin\NET40\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Json.dll .\src\FullKey.snk
