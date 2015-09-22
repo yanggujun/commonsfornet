d739153a45b7e640ba7d8d5dc10f3931a0ad04d6
yanggujun <gujun.yang@gmail.com>
2015-09-22 20:41:59 +0800

    Prepare for release 0.1.1.219

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 //   build number is increased by one if the number of days to the first day is less than or 
 //   equal to the build number.
 // * TODO: Automated version strategy will be developed in future.
-[assembly: AssemblyVersion("0.1.1.22")]
-[assembly: AssemblyFileVersion("0.1.1.22")]
+[assembly: AssemblyVersion("0.1.1.219")]
+[assembly: AssemblyFileVersion("0.1.1.219")]
 [assembly: CLSCompliant(true)]
