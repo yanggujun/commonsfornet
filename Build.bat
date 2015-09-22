70d934924757196d2f95ab29282ad4d4b3788c59
yanggujun <gujun.yang@gmail.com>
2015-09-22 21:28:08 +0800

    merge 0.1.1.219. update Commons.Pool to support both .net 4.0 and .ent 4.5

--- Build.bat
+++ Build.bat
@@ -6,12 +6,14 @@ call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any C
 call sn -R .\src\bin\NET45\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Json.dll .\src\FullKey.snk
+call sn -R .\src\bin\NET45\Commons.Pool.dll .\src\FullKey.snk
 call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\NET45\Test.Commons.dll /xml src\bin\NET45\UnitTestResult.xml
 
 call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";Framework=NET40
 call sn -R .\src\bin\NET40\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Json.dll .\src\FullKey.snk
+call sn -R .\src\bin\NET40\Commons.Pool.dll .\src\FullKey.snk
 call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\NET40\Test.Commons.dll /xml src\bin\NET40\UnitTestResult.xml
 
 call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"
