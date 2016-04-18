72b6519341aabdc2fbba1497df579b1466c5349b
yanggujun <gujun.yang@gmail.com>
2016-04-18 21:43:49 +0800

    try to enable ci build

--- Build.bat
+++ Build.bat
@@ -8,14 +8,14 @@ call sn -R .\src\bin\NET45\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Json.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET45\Commons.Pool.dll .\src\FullKey.snk
-call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\NET45\Test.Commons.dll /xml src\bin\NET45\UnitTestResult.xml
+call src\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe src\bin\NET45\Test.Commons.dll 
 
 call msbuild src\Commons.sln /t:Rebuild /p:Configuration=Release;Platform="Any CPU";Framework=NET40
 call sn -R .\src\bin\NET40\Commons.Utils.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Collections.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Json.dll .\src\FullKey.snk
 call sn -R .\src\bin\NET40\Commons.Pool.dll .\src\FullKey.snk
-call src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe src\bin\NET40\Test.Commons.dll /xml src\bin\NET40\UnitTestResult.xml
+call src\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe src\bin\NET40\Test.Commons.dll
 
 call nuget pack Commons.nuspec -OutputDirectory ".\src\bin"
 call nuget pack Commons.Pool.nuspec -OutputDirectory ".\src\bin"
