28f9e9a111da52adc2388d0e43d38bdbd0f904ac
yanggujun <gujun.yang@gmail.com>
2016-06-07 23:28:37 +0800

    build on ubuntu

--- Build.NetCore.sh
+++ Build.NetCore.sh
@@ -1,35 +1,37 @@
-set OUTPUT=src\artifacts\binaries
+#!/bin/sh
 
-call rmdir /S /Q ".\src\artifacts"
+OUTPUT=./src/artifacts/binaries
 
-call dotnet restore 
+rm -r ./src/artifacts
 
-call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
-call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
-call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
-call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
-call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
+dotnet restore 
 
-call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
-call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
-call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
-call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
-call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
+dotnet build src/Commons.Utils -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
+dotnet build src/Commons.Collections -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
+dotnet build src/Commons.Json -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
+dotnet build src/Commons.Pool -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
+dotnet build src/Test.Commons -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
 
-call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
-call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
-call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
-call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
+dotnet build src/Commons.Utils -c Release --no-dependencies -o "$OUTPUT/net45" -f net45 --no-incremental
+dotnet build src/Commons.Collections -c Release --no-dependencies -o "$OUTPUT/net45" -f net45 --no-incremental
+dotnet build src/Commons.Json -c Release --no-dependencies -o "$OUTPUT/net45" -f net45 --no-incremental
+dotnet build src/Commons.Pool -c Release --no-dependencies -o "$OUTPUT/net45" -f net45 --no-incremental
+dotnet build src/Test.Commons -c Release --no-dependencies -o "$OUTPUT/net45" -f net45 --no-incremental
 
-call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\netcoreapp1.0" -f netcoreapp1.0 --no-incremental
+dotnet build src/Commons.Utils -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental
+dotnet build src/Commons.Collections -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental
+dotnet build src/Commons.Json -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental
+dotnet build src/Commons.Pool -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental
 
-call dotnet test src\Test.Commons -f netcoreapp1.0 -o "%OUTPUT%\netcoreapp1.0" --no-build
-call %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe %OUTPUT%\net40\Test.Commons.dll
-call %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe %OUTPUT%\net45\Test.Commons.dll
+dotnet build src/Test.Commons -c Release --no-dependencies -o "$OUTPUT/netcoreapp1.0" -f netcoreapp1.0 --no-incremental
 
-.\src\.nuget\nuget pack Commons.nuspec -outputdirectory %OUTPUT%
-.\src\.nuget\nuget pack Commons.Json.nuspec -outputdirectory %OUTPUT%
-.\src\.nuget\nuget pack Commons.Pool.nuspec -outputdirectory %OUTPUT%
+dotnet test src/Test.Commons -f netcoreapp1.0 -o "$OUTPUT/netcoreapp1.0" --no-build
+%USERPROFILE%/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe $OUTPUT/net40/Test.Commons.dll
+%USERPROFILE%/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe $OUTPUT/net45/Test.Commons.dll
+
+dotnet run ./src/.nuget/nuget.exe pack Commons.nuspec -outputdirectory $OUTPUT
+dotnet run ./src/.nuget/nuget.exe pack Commons.Json.nuspec -outputdirectory $OUTPUT
+dotnet run ./src/.nuget/nuget.exe pack Commons.Pool.nuspec -outputdirectory $OUTPUT
 
 
-call PAUSE
+PAUSE
