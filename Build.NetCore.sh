7b846fc0bd2fecbfaa37106291fd61e766a31237
yanggujun <gujun.yang@gmail.com>
2016-06-07 21:48:28 +0800

    update nuget version, modify build sequence

--- Build.NetCore.sh
+++ Build.NetCore.sh
@@ -10,28 +10,22 @@ call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\net
 call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
 call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
 
-
 call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
 call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
 call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
 call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
 call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\net45" -f net45 --no-incremental
 
-
 call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
 call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
 call dotnet build src\Commons.Json -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
 call dotnet build src\Commons.Pool -c Release --no-dependencies -o "%OUTPUT%\netstandard1.3" -f netstandard1.3 --no-incremental
 
-
 call dotnet build src\Test.Commons -c Release --no-dependencies -o "%OUTPUT%\netcoreapp1.0" -f netcoreapp1.0 --no-incremental
 
-call pushd %OUTPUT%\netcoreapp1.0
-call dotnet test ..\..\..\Test.Commons -o .\ --no-build -f netcoreapp1.0
-call popd
-
-call "%USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net40\Test.Commons.dll
-call "%USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net45\Test.Commons.dll
+call dotnet test src\Test.Commons -f netcoreapp1.0 -o "%OUTPUT%\netcoreapp1.0" --no-build
+call %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe %OUTPUT%\net40\Test.Commons.dll
+call %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe %OUTPUT%\net45\Test.Commons.dll
 
 .\src\.nuget\nuget pack Commons.nuspec -outputdirectory %OUTPUT%
 .\src\.nuget\nuget pack Commons.Json.nuspec -outputdirectory %OUTPUT%
