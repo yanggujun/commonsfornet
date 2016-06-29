3dfd629c5cbf61d79ba1632708af328fd5cabf97
yanggujun <alanier@sina.cn>
2016-06-29 14:26:03 +0800

    specify nuget package restore location

--- Build.bat
+++ Build.bat
@@ -4,7 +4,7 @@ call rmdir /S /Q ".\src\artifacts"
 
 call dotnet --info
 
-call dotnet restore 
+call dotnet restore --packages .\src\packages
 
 call dotnet build src\Commons.Utils -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
 call dotnet build src\Commons.Collections -c Release --no-dependencies -o "%OUTPUT%\net40" -f net40 --no-incremental
@@ -32,8 +32,8 @@ call pushd %OUTPUT%\netcoreapp1.0
 call dotnet test ..\..\..\Test.Commons -o .\ --no-build -f netcoreapp1.0 -parallel none 
 call popd
 
-call "%USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net40\Test.Commons.dll -parallel none 
-call "%USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net45\Test.Commons.dll -parallel none 
+call ".\src\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net40\Test.Commons.dll -parallel none 
+call ".\src\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe" %OUTPUT%\net45\Test.Commons.dll -parallel none 
 
 .\src\.nuget\nuget pack Commons.nuspec -outputdirectory %OUTPUT%
 .\src\.nuget\nuget pack Commons.Json.nuspec -outputdirectory %OUTPUT%
