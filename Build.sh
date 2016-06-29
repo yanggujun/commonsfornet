86a5b2fb92fc1273ff297bfc144f6a63309773c1
yanggujun <alanier@sina.cn>
2016-06-29 14:59:02 +0800

    use home location for mono test

--- Build.sh
+++ Build.sh
@@ -4,7 +4,7 @@ rm -r ./src/artifacts
 
 dotnet --info
 
-dotnet restore --packages ./src/packages
+dotnet restore --packages
 
 dotnet build src/Commons.Utils -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
 dotnet build src/Commons.Collections -c Release --no-dependencies -o "$OUTPUT/net40" -f net40 --no-incremental
@@ -31,9 +31,5 @@ cd $OUTPUT/netcoreapp1.0
 dotnet test ../../../Test.Commons -o ./ --no-build -f netcoreapp1.0 -parallel none 
 cd ../../../../
 
-mono "./src/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe" $OUTPUT/net40/Test.Commons.dll -parallel none 
-mono "./src/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe" $OUTPUT/net45/Test.Commons.dll -parallel none 
-
-mono ./src/.nuget/NuGet.exe pack Commons.nuspec -outputdirectory $OUTPUT
-mono ./src/.nuget/NuGet.exe pack Commons.Json.nuspec -outputdirectory $OUTPUT
-mono ./src/.nuget/NuGet.exe pack Commons.Pool.nuspec -outputdirectory $OUTPUT
+mono "$HOME/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe" $OUTPUT/net40/Test.Commons.dll -parallel none 
+mono "$HOME/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console.exe" $OUTPUT/net45/Test.Commons.dll -parallel none 
