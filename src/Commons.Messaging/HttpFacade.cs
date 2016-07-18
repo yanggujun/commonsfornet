50f49041f9ab61abddd88f4bc5ba28be22a4fb33
yanggujun <gujun.yang@gmail.com>
2016-07-18 23:10:36 +0800

    http handler

--- src/Commons.Messaging/HttpFacade.cs
+++ src/Commons.Messaging/HttpFacade.cs
@@ -15,16 +15,39 @@
 // limitations under the License.
 
 using System;
+using System.Reflection;
+using System.Text;
+using Commons.Collections.Queue;
+using Commons.Json;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace Commons.Messaging
 {
     [CLSCompliant(false)]
-    public class HttpFacade
+    public class HttpFacade<T>
     {
+        private MemQueue<T> queue = new MemQueue<T>(typeof(T).GetType().ToString(), x => Console.WriteLine(x));
+
         public void OnHttpRequest(HttpContext context)
         {
-
+            StringValues strValues;
+            if (context.Request.Headers.TryGetValue(Contants.MessageType, out strValues))
+            {
+                if (strValues.Count > 1)
+                {
+                    var assemblyName = strValues[0];
+                    var typeName = strValues[0];
+                    var assembly = Assembly.Load(new AssemblyName(assemblyName));
+                    var type = assembly.GetType(typeName);
+                    var length = context.Request.Body.Length;
+                    var buffer = new byte[length];
+                    context.Request.Body.Read(buffer, 0, (int)length);
+                    var json = Encoding.Unicode.GetString(buffer);
+                    var message = (T)JsonMapper.To(type, json);
+                    queue.Enqueue(message);
+                }
+            }
         }
     }
 }
