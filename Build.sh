0cdfac8d5d91936884985a0b557873f960cde237
yanggujun <yanggujun@yanggujun-debian>
2017-03-04 04:28:21 -0500

    add Commons.Perf to bash build

--- Build.sh
+++ Build.sh
@@ -8,13 +8,8 @@ dotnet --info
 dotnet restore
 
 sh ./tools/_build.sh netstandard1.3
-sh ./tools/_build.sh net40
-sh ./tools/_build.sh net45
-
 
 sh ./tools/_test.sh netcoreapp1.0
-sh ./tools/_test.sh net40
-sh ./tools/_test.sh net45
 
 cd $OUTPUT/netcoreapp1.0
 dotnet Commons.Perf.dll
--- tools/_build.sh
+++ tools/_build.sh
@@ -13,4 +13,6 @@ dotnet build src/Commons.Pool -c Release --no-dependencies -o "$OUTPUT/$framewor
 dotnet build src/Commons.Json -c Release --no-dependencies -o "$OUTPUT/$framework" -f $framework --no-incremental --build-profile
 dotnet build src/Commons.Messaging -c Release --no-dependencies -o "$OUTPUT/$framework" -f $framework --no-incremental --build-profile
 dotnet build src/Commons.Test -c Release --no-dependencies -o "$OUTPUT/$testFramework" -f $testFramework --no-incremental --build-profile
+dotnet build src/Commons.Perf -c Release --no-dependencies -o "$OUTPUT/$testFramework" -f $testFramework --no-incremental --build-profile
+
 
