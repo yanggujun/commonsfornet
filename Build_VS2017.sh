17ea2c6b3bea9aeef93838cd484436a8fca36090
yanggujun <gujun.yang@gmail.com>
2017-04-10 20:51:28 +0800

    try to build on dotnet core runtime 1.0.4 lts

---
+++ Build_VS2017.sh
@@ -0,0 +1,41 @@
+$OUTPUT= $PWD/src/artifacts/rbin
+$Artifacts=$PWD/src/artifacts
+$src=$PWD/src
+
+if [ -d "$Artifacts" ]; then
+    rm -rf $Artifacts
+fi
+
+
+$version = "0.3.0.0"
+
+echo $version
+
+dotnet --info
+dotnet restore $src/Commons.Utils/Commons.Utils.csproj
+dotnet restore $src/Commons.Collections/Commons.Collections.csproj
+dotnet restore $src/Commons.Reflect/Commons.Reflect.csproj
+dotnet restore $src/Commons.Pool/Commons.Pool.csproj
+dotnet restore $src/Commons.Json/Commons.Json.csproj
+dotnet restore $src/Commons.Messaging/Commons.Messaging.csproj
+dotnet restore $src/Commons.Test/Commons.Test.csproj
+dotnet restore $src/Commons.Perf/Commons.Perf.csproj
+
+
+dotnet build $src/Commons.Utils/Commons.Utils.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Collections/Commons.Collections.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Reflect/Commons.Reflect.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Pool/Commons.Pool.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Json/Commons.Json.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Messaging/Commons.Messaging.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Test/Commons.Test.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netcoreapp1.0 --no-incremental 
+dotnet build $src/Commons.Perf/Commons.Perf.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netcoreapp1.0 --no-incremental 
+
+pushd $OUTPUT/netstandard1.3
+dotnet test $src/Commons.Test/Commons.Test.csproj -o $OUTPUT/netstandard1.3 --no-build -f netcoreapp1.0
+popd
+
+
+pushd $OUTPUT/netstandard1.3
+dotnet Commons.Perf.dll
+popd
