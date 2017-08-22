21ec1c9d34f3265b70e020a5eee32699bb62422c
yanggujun <gujun.yang@gmail.com>
2017-08-22 22:02:58 +0800

    Remove VS2015 sln

--- Build.sh
+++ Build.sh
@@ -1,16 +1,34 @@
+export OUTPUT=$PWD/src/artifacts/rbin
+export Artifacts=$PWD/src/artifacts
+export src=$PWD/src
 
-. ./tools/_env.sh
+if [ -d "$Artifacts" ]; then
+    rm -rf $Artifacts
+fi
 
-rm -r ./src/artifacts
 
 dotnet --info
 
-dotnet restore
+dotnet restore $src/Commons.Utils/Commons.Utils.csproj
+dotnet restore $src/Commons.Collections/Commons.Collections.csproj
+dotnet restore $src/Commons.Reflect/Commons.Reflect.csproj
+dotnet restore $src/Commons.Pool/Commons.Pool.csproj
+dotnet restore $src/Commons.Json/Commons.Json.csproj
+dotnet restore $src/Commons.Messaging/Commons.Messaging.csproj
+dotnet restore $src/Commons.Test/Commons.Test.csproj
+dotnet restore $src/Commons.Perf/Commons.Perf.csproj
 
-sh ./tools/_build.sh netstandard1.3
 
-sh ./tools/_test.sh netcoreapp1.0
+dotnet build $src/Commons.Utils/Commons.Utils.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Collections/Commons.Collections.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Reflect/Commons.Reflect.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Pool/Commons.Pool.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Json/Commons.Json.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Messaging/Commons.Messaging.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netstandard1.3 --no-incremental 
+dotnet build $src/Commons.Test/Commons.Test.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netcoreapp1.0 --no-incremental 
+dotnet build $src/Commons.Perf/Commons.Perf.csproj -c Release --no-dependencies -o "$OUTPUT/netstandard1.3" -f netcoreapp1.0 --no-incremental 
 
-cd $OUTPUT/netcoreapp1.0
+cd $OUTPUT/netstandard1.3
+dotnet test $src/Commons.Test/Commons.Test.csproj -o $OUTPUT/netstandard1.3 --no-build -f netcoreapp1.0
+
 dotnet Commons.Perf.dll
-cd ../../../../
